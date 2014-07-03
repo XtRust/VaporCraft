27f773596e5f0f73472ad79f31e9751e36e14905
Anzóis 1 <jo.nu.bento@gmail.com>
2014-07-03 15:35:18 +0100

    first WIP stuff

---
+++ src/main/java/com/XtRust/VaporCraft/VaporCraft.java
@@ -0,0 +1,15 @@
+package com.XtRust.VaporCraft;
+
+import cpw.mods.fml.common.Mod;
+
+
+/*
+Just The normal stuff for the mod file
+*/
+
+@Mod(modid = "VaporCraft", name = "VaporCraft", version = "1.7.2-0.1 [WIP]")
+
+public class VaporCraft {
+
+
+}
